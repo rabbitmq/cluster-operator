738761d4fdea77e00d8bfe6f3026434fe8e78a65
Chunyi Lyu <clyu@pivotal.io>
2019-06-13 16:38:35 +0100

    Store and use keys and creds in lastpass
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-fly -t rmq4k8s set-pipeline -p operator -c pipeline.yml -l vars.yml
+fly -t rmq set-pipeline -p operator -c pipeline.yml \
+  -v git-ssh-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-git-ssh-key" --note)" \
+  -v gcr-push-service-account-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-gcr-push" --note)" \
+  -v rmq-k8s-ci-kubectl-service-account-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-kubectl-service-account-key" --note)"
