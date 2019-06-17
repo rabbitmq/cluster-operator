181e570601cd42809fac3d603bfd62d50b33f23e
Chunyi Lyu <clyu@pivotal.io>
2019-06-17 15:33:30 +0100

    Add 'renew-env' job the renew toolsmith envs
    [#166573817]

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -4,5 +4,6 @@ set -e
 
 fly -t rmq set-pipeline -p operator -c pipeline.yml \
   -v git-ssh-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-git-ssh-key" --note)" \
-  -v gcr-push-service-account-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-gcr-push" --note)" \
-  -v rmq-k8s-ci-kubectl-service-account-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-kubectl-service-account-key" --note)"
+  -v gcr-push-service-account-key="$(lpass show "Shared-RabbitMQ for Kubernetes/ci-gcr-access" --note)" \
+  -v rmq-k8s-ci-kubectl-service-account-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-kubectl-service-account-key" --note)" \
+  -v toolsmith-api-token="$(lpass show "Shared-RabbitMQ for Kubernetes/Toolsmith API Token" --note)"
