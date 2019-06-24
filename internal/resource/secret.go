bd3ea4a36c91390f04c11f74345b9dcf4e5bb5ea
Chunyi Lyu <clyu@pivotal.io>
2019-06-24 18:01:56 +0100

    Add GenerateSecret() in resource package
    [#164656685]

---
+++ internal/resource/secret.go
@@ -0,0 +1,42 @@
+package resource
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	rabbitmqv1beta1 "github.com/pivotal/rabbitmq-for-kubernetes/api/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func GenerateSecret(instance rabbitmqv1beta1.RabbitmqCluster) (*corev1.Secret, error) {
+	username, err := randomEncodedString(24)
+	if err != nil {
+		return nil, err
+	}
+
+	password, err := randomEncodedString(24)
+	if err != nil {
+		return nil, err
+	}
+
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "rabbitmq-secret",
+			Namespace: instance.Namespace,
+		},
+		Type: corev1.SecretTypeOpaque,
+		Data: map[string][]byte{
+			"rabbitmq-username": []byte(username),
+			"rabbitmq-password": []byte(password),
+		},
+	}, nil
+}
+
+func randomEncodedString(dataLen int) (string, error) {
+	randomBytes := make([]byte, dataLen)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(randomBytes), nil
+}
---
+++ internal/resource/secret_test.go
@@ -0,0 +1,56 @@
+package resource_test
+
+import (
+	b64 "encoding/base64"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	rabbitmqv1beta1 "github.com/pivotal/rabbitmq-for-kubernetes/api/v1beta1"
+	"github.com/pivotal/rabbitmq-for-kubernetes/internal/resource"
+	corev1 "k8s.io/api/core/v1"
+)
+
+var _ = Describe("Secret", func() {
+	var instance rabbitmqv1beta1.RabbitmqCluster
+	var secret *corev1.Secret
+	var err error
+
+	BeforeEach(func() {
+		instance = rabbitmqv1beta1.RabbitmqCluster{}
+		instance.Namespace = "foo"
+		instance.Name = "foo"
+	})
+
+	Context("when succeeds", func() {
+		BeforeEach(func() {
+			secret, err = resource.GenerateSecret(instance)
+			Expect(err).NotTo(HaveOccurred())
+
+		})
+
+		It("creates the secret 'rabbitmq-secret'", func() {
+			Expect(secret.Name).To(Equal("rabbitmq-secret"))
+		})
+
+		It("creates a 'opaque' secret ", func() {
+			Expect(secret.Type).To(Equal(corev1.SecretTypeOpaque))
+		})
+
+		It("creates a rabbitmq username that is base64 encoded and 24 characters in length", func() {
+			username, ok := secret.Data["rabbitmq-username"]
+			Expect(ok).NotTo(BeFalse())
+			decodedUsername, err := b64.URLEncoding.DecodeString(string(username))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(decodedUsername)).To(Equal(24))
+
+		})
+
+		It("creates a rabbitmq password that is base64 encoded and 24 characters in length", func() {
+			password, ok := secret.Data["rabbitmq-password"]
+			Expect(ok).NotTo(BeFalse())
+			decodedPassword, err := b64.URLEncoding.DecodeString(string(password))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(decodedPassword)).To(Equal(24))
+
+		})
+	})
+})
