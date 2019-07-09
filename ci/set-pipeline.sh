32a6e124649b4534c9390a1460569234b5077c12
Chunyi Lyu <clyu@pivotal.io>
2019-07-09 12:30:00 +0100

    Change lpass refs and update pipeline variables
    [#167063472]
    Co-authored-by: Jonathan Tsang <jtsang@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -3,7 +3,8 @@
 set -e
 
 fly -t rmq set-pipeline -p operator -c pipeline.yml \
-  -v git-ssh-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-git-ssh-key" --note)" \
-  -v gcr-push-service-account-key="$(lpass show "Shared-RabbitMQ for Kubernetes/ci-gcr-access" --note)" \
-  -v rmq-k8s-ci-kubectl-service-account-key="$(lpass show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-kubectl-service-account-key" --note)" \
-  -v toolsmith-api-token="$(lpass show "Shared-RabbitMQ for Kubernetes/Toolsmith API Token" --note)"
+  -v git-ssh-key="$(lpassd show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-git-ssh-key" --note)" \
+  -v gcr-push-service-account-key="$(lpassd show "Shared-RabbitMQ for Kubernetes/ci-gcr-access" --note | jq -c .)" \
+  -v gcr-pull-service-account-key="$(lpassd show "Shared-RabbitMQ for Kubernetes/ci-gcr-pull" --note | jq -c .)" \
+  -v rmq-k8s-ci-kubectl-service-account-key="$(lpassd show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-kubectl-service-account-key" --note | jq -c .)" \
+  -v toolsmith-api-token="$(lpassd show 'Shared-RabbitMQ for Kubernetes/toolsmiths-api-token' --password)"
