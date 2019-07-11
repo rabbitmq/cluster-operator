6dd592e33d710876d07d238708f980449e5ac92e
Chunyi Lyu <clyu@pivotal.io>
2019-07-11 11:27:45 +0100

    Temp concourse job to upload to pivnet
    [#166983670]
    Co-authored-by: Jonathan Tsang <jtsang@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -7,4 +7,8 @@ fly -t rmq set-pipeline -p operator -c pipeline.yml \
   -v gcr-push-service-account-key="$(lpassd show "Shared-RabbitMQ for Kubernetes/ci-gcr-access" --note | jq -c .)" \
   -v gcr-pull-service-account-key="$(lpassd show "Shared-RabbitMQ for Kubernetes/ci-gcr-pull" --note | jq -c .)" \
   -v rmq-k8s-ci-kubectl-service-account-key="$(lpassd show "Shared-RabbitMQ for Kubernetes/rmq-k8s-ci-kubectl-service-account-key" --note | jq -c .)" \
+  -v pivnet-api-token="$(lpassd show "Shared-RabbitMQ for Kubernetes/pivnet-api-token" --password)" \
+  -v pivnet-aws-access-key-id="$(lpassd show "Shared-RabbitMQ for Kubernetes/rabbitmq-for-kubernetes-pivnet-s3-user" --username)" \
+  -v pivnet-aws-secret-access-key="$(lpassd show "Shared-RabbitMQ for Kubernetes/rabbitmq-for-kubernetes-pivnet-s3-user" --password)" \
+  -v release-metadata-content="$(lpassd show "Shared-RabbitMQ for Kubernetes/pivnet-release-metadata" --notes)" \
   -v toolsmith-api-token="$(lpassd show 'Shared-RabbitMQ for Kubernetes/toolsmiths-api-token' --password)"
