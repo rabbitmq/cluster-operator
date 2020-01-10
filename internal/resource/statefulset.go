9acb7b3584cafff9fdeb45e1d3b53d787d8e34fd
Chunyi Lyu <clyu@pivotal.io>
2020-01-10 15:01:53 +0000

    Use Quantity.Equal to compare memory limit and request
    - we used to use '!=' which is not suitable for comparing
    Quantity objects
    
    [#170338584]

--- internal/resource/statefulset.go
+++ internal/resource/statefulset.go
@@ -80,7 +80,7 @@ func (builder *StatefulSetBuilder) Build() (runtime.Object, error) {
 		return nil, fmt.Errorf("failed setting controller reference: %v", err)
 	}
 
-	if sts.Spec.Template.Spec.Containers[0].Resources.Limits.Memory() != sts.Spec.Template.Spec.Containers[0].Resources.Requests.Memory() {
+	if !sts.Spec.Template.Spec.Containers[0].Resources.Limits.Memory().Equal(*sts.Spec.Template.Spec.Containers[0].Resources.Requests.Memory()) {
 		logger := ctrl.Log.WithName("statefulset").WithName("RabbitmqCluster")
 		logger.Info(fmt.Sprintf("Warning: Memory request and limit are not equal for \"%s\". It is recommended that they be set to the same value", sts.GetName()))
 	}
