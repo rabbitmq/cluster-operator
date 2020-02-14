65461da31e944e33c3a4908d882001532593d37a
James McNeil <jmcneil@pivotal.io>
2020-02-14 16:27:56 +0000

    Remove redundant label and annotation setting
    - Update gets called on resource creation so there is no need to set
    theme in both functions
    
    [#170975588]
    
    Co-authored-by: Jonathan Tsang <jtsang@pivotal.io>

--- internal/resource/service_account.go
+++ internal/resource/service_account.go
@@ -32,10 +32,8 @@ func (builder *ServiceAccountBuilder) Update(object runtime.Object) error {
 func (builder *ServiceAccountBuilder) Build() (runtime.Object, error) {
 	return &corev1.ServiceAccount{
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace:   builder.Instance.Namespace,
-			Name:        builder.Instance.ChildResourceName(serviceAccountName),
-			Labels:      metadata.GetLabels(builder.Instance.Name, builder.Instance.Labels),
-			Annotations: metadata.ReconcileAnnotations(map[string]string{}, builder.Instance.Annotations),
+			Namespace: builder.Instance.Namespace,
+			Name:      builder.Instance.ChildResourceName(serviceAccountName),
 		},
 	}, nil
 }
--- internal/resource/service_account_test.go
+++ internal/resource/service_account_test.go
@@ -31,7 +31,7 @@ var _ = Describe("ServiceAccount", func() {
 		serviceAccountBuilder = builder.ServiceAccount()
 	})
 
-	Context("Build with defaults", func() {
+	Context("Build", func() {
 		BeforeEach(func() {
 			obj, err := serviceAccountBuilder.Build()
 			serviceAccount = obj.(*corev1.ServiceAccount)
@@ -42,147 +42,92 @@ var _ = Describe("ServiceAccount", func() {
 			Expect(serviceAccount.Name).To(Equal(builder.Instance.ChildResourceName("server")))
 			Expect(serviceAccount.Namespace).To(Equal(builder.Instance.Namespace))
 		})
-
-		It("only creates the required labels", func() {
-			labels := serviceAccount.Labels
-			Expect(len(labels)).To(Equal(3))
-			Expect(labels["app.kubernetes.io/name"]).To(Equal(instance.Name))
-			Expect(labels["app.kubernetes.io/component"]).To(Equal("rabbitmq"))
-			Expect(labels["app.kubernetes.io/part-of"]).To(Equal("pivotal-rabbitmq"))
-		})
 	})
 
-	Context("Build with instance labels", func() {
-		BeforeEach(func() {
-			instance.Labels = map[string]string{
-				"app.kubernetes.io/foo": "bar",
-				"foo":                   "bar",
-				"rabbitmq":              "is-great",
-				"foo/app.kubernetes.io": "edgecase",
-			}
-
-			obj, err := serviceAccountBuilder.Build()
-			Expect(err).NotTo(HaveOccurred())
-			serviceAccount = obj.(*corev1.ServiceAccount)
-		})
-
-		It("has the labels from the CRD on the serviceAccount", func() {
-			testLabels(serviceAccount.Labels)
-		})
-
-		It("also has the required labels", func() {
-			labels := serviceAccount.Labels
-			Expect(labels["app.kubernetes.io/name"]).To(Equal(instance.Name))
-			Expect(labels["app.kubernetes.io/component"]).To(Equal("rabbitmq"))
-			Expect(labels["app.kubernetes.io/part-of"]).To(Equal("pivotal-rabbitmq"))
-		})
-	})
-
-	Context("Build with instance annotations", func() {
-		BeforeEach(func() {
-			instance.Annotations = map[string]string{
-				"my-annotation":              "i-like-this",
-				"kubernetes.io/name":         "i-do-not-like-this",
-				"kubectl.kubernetes.io/name": "i-do-not-like-this",
-				"k8s.io/name":                "i-do-not-like-this",
-			}
-
-			obj, err := serviceAccountBuilder.Build()
-			Expect(err).NotTo(HaveOccurred())
-			serviceAccount = obj.(*corev1.ServiceAccount)
-		})
-
-		It("has the annotations from the CRD on the serviceAccount", func() {
-			expectedAnnotations := map[string]string{
-				"my-annotation": "i-like-this",
-			}
-
-			Expect(serviceAccount.Annotations).To(Equal(expectedAnnotations))
-		})
-	})
-
-	Context("Update with instance labels", func() {
-		BeforeEach(func() {
-			instance = rabbitmqv1beta1.RabbitmqCluster{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "rabbit-labelled",
-				},
-			}
-			instance.Labels = map[string]string{
-				"app.kubernetes.io/foo": "bar",
-				"foo":                   "bar",
-				"rabbitmq":              "is-great",
-				"foo/app.kubernetes.io": "edgecase",
-			}
-
-			serviceAccount = &corev1.ServiceAccount{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app.kubernetes.io/name":      instance.Name,
-						"app.kubernetes.io/part-of":   "pivotal-rabbitmq",
-						"this-was-the-previous-label": "should-be-deleted",
+	Context("Update", func() {
+		Context("instance labels", func() {
+			BeforeEach(func() {
+				instance = rabbitmqv1beta1.RabbitmqCluster{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rabbit-labelled",
 					},
-				},
-			}
-			err := serviceAccountBuilder.Update(serviceAccount)
-			Expect(err).NotTo(HaveOccurred())
-		})
+				}
+				instance.Labels = map[string]string{
+					"app.kubernetes.io/foo": "bar",
+					"foo":                   "bar",
+					"rabbitmq":              "is-great",
+					"foo/app.kubernetes.io": "edgecase",
+				}
 
-		It("adds labels from the CR", func() {
-			testLabels(serviceAccount.Labels)
-		})
-
-		It("restores the default labels", func() {
-			labels := serviceAccount.Labels
-			Expect(labels["app.kubernetes.io/name"]).To(Equal(instance.Name))
-			Expect(labels["app.kubernetes.io/component"]).To(Equal("rabbitmq"))
-			Expect(labels["app.kubernetes.io/part-of"]).To(Equal("pivotal-rabbitmq"))
-		})
-
-		It("deletes the labels that are removed from the CR", func() {
-			Expect(serviceAccount.Labels).NotTo(HaveKey("this-was-the-previous-label"))
-		})
-	})
-
-	Context("Update with instance annotations", func() {
-		BeforeEach(func() {
-			instance = rabbitmqv1beta1.RabbitmqCluster{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "rabbit-labelled",
-				},
-			}
-			instance.Annotations = map[string]string{
-				"my-annotation":              "i-like-this",
-				"kubernetes.io/name":         "i-do-not-like-this",
-				"kubectl.kubernetes.io/name": "i-do-not-like-this",
-				"k8s.io/name":                "i-do-not-like-this",
-			}
-
-			serviceAccount = &corev1.ServiceAccount{
-				ObjectMeta: metav1.ObjectMeta{
-					Annotations: map[string]string{
-						"old-annotation":                "old-value",
-						"im-here-to-stay.kubernetes.io": "for-a-while",
-						"kubernetes.io/name":            "should-stay",
-						"kubectl.kubernetes.io/name":    "should-stay",
-						"k8s.io/name":                   "should-stay",
+				serviceAccount = &corev1.ServiceAccount{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"app.kubernetes.io/name":      instance.Name,
+							"app.kubernetes.io/part-of":   "pivotal-rabbitmq",
+							"this-was-the-previous-label": "should-be-deleted",
+						},
 					},
-				},
-			}
-			err := serviceAccountBuilder.Update(serviceAccount)
-			Expect(err).NotTo(HaveOccurred())
+				}
+				err := serviceAccountBuilder.Update(serviceAccount)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("adds labels from the CR", func() {
+				testLabels(serviceAccount.Labels)
+			})
+
+			It("restores the default labels", func() {
+				labels := serviceAccount.Labels
+				Expect(labels["app.kubernetes.io/name"]).To(Equal(instance.Name))
+				Expect(labels["app.kubernetes.io/component"]).To(Equal("rabbitmq"))
+				Expect(labels["app.kubernetes.io/part-of"]).To(Equal("pivotal-rabbitmq"))
+			})
+
+			It("deletes the labels that are removed from the CR", func() {
+				Expect(serviceAccount.Labels).NotTo(HaveKey("this-was-the-previous-label"))
+			})
 		})
 
-		It("updates annotations on service account", func() {
-			expectedAnnotations := map[string]string{
-				"my-annotation":                 "i-like-this",
-				"old-annotation":                "old-value",
-				"im-here-to-stay.kubernetes.io": "for-a-while",
-				"kubernetes.io/name":            "should-stay",
-				"kubectl.kubernetes.io/name":    "should-stay",
-				"k8s.io/name":                   "should-stay",
-			}
-			Expect(serviceAccount.Annotations).To(Equal(expectedAnnotations))
+		Context("instance annotations", func() {
+			BeforeEach(func() {
+				instance = rabbitmqv1beta1.RabbitmqCluster{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "rabbit-labelled",
+					},
+				}
+				instance.Annotations = map[string]string{
+					"my-annotation":              "i-like-this",
+					"kubernetes.io/name":         "i-do-not-like-this",
+					"kubectl.kubernetes.io/name": "i-do-not-like-this",
+					"k8s.io/name":                "i-do-not-like-this",
+				}
+
+				serviceAccount = &corev1.ServiceAccount{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{
+							"old-annotation":                "old-value",
+							"im-here-to-stay.kubernetes.io": "for-a-while",
+							"kubernetes.io/name":            "should-stay",
+							"kubectl.kubernetes.io/name":    "should-stay",
+							"k8s.io/name":                   "should-stay",
+						},
+					},
+				}
+				err := serviceAccountBuilder.Update(serviceAccount)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("annotations on service account", func() {
+				expectedAnnotations := map[string]string{
+					"my-annotation":                 "i-like-this",
+					"old-annotation":                "old-value",
+					"im-here-to-stay.kubernetes.io": "for-a-while",
+					"kubernetes.io/name":            "should-stay",
+					"kubectl.kubernetes.io/name":    "should-stay",
+					"k8s.io/name":                   "should-stay",
+				}
+				Expect(serviceAccount.Annotations).To(Equal(expectedAnnotations))
+			})
 		})
 	})
 })
