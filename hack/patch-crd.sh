336d802ed4ab2b16a7e0030e3acdb19ab658270c
Chunyi Lyu <clyu@pivotal.io>
2020-11-18 14:50:15 +0000

    Remove crd patch hack
    - new controller-gen now adds default values to ports
    the hack is no longer needed

--- hack/patch-crd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl patch -f config/crd/bases/rabbitmq.com_rabbitmqclusters.yaml -p "$(cat hack/crd-patch.json)" --type json --local  -o yaml > /tmp/rabbitmq.com_rabbitmqclusters.yaml
-mv /tmp/rabbitmq.com_rabbitmqclusters.yaml config/crd/bases/rabbitmq.com_rabbitmqclusters.yaml
