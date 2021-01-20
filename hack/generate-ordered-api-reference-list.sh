176c9014e24bb6b388963b38c9f7139bb6f4a92a
Connor Rogers <rconnor@vmware.com>
2021-01-20 11:25:34 +0000

    Add versioning to autogenerated API docs (#561)
    * Add scripts to regenerate the Wiki sidebar doc on new API versions
    
    * Fix generated URLs to not have the .asciidoc suffix
    
    This allows linking to the wiki pages rather than the raw asciidoc file

---
+++ hack/generate-ordered-api-reference-list.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+wikiDir=$1
+
+generateVersionedEntry() {
+  echo "* [$1](https://github.com/rabbitmq/cluster-operator/wiki/API_Reference_$1)"
+}
+
+generateLatestEntry() {
+  echo "* [Latest](https://github.com/rabbitmq/cluster-operator/wiki/API_Reference)"
+}
+
+lead='^<!--- BEGIN API REFERENCE LIST -->$'
+tail='^<!--- END API REFERENCE LIST -->$'
+
+unorderedlistfile="$(mktemp)"
+orderedlistfile="$(mktemp)"
+
+apiVersions="$(find $wikiDir/API_Reference_* -printf "%f\n" | sed -r 's/API_Reference_(v[0-9]+.[0-9]+.[0-9]+).asciidoc/\1/' | sort -Vr )"
+for version in $apiVersions
+do
+  echo "$(generateVersionedEntry $version)" >> $unorderedlistfile
+done
+
+# Latest API version is a special case, that is always top of the list
+generateLatestEntry > $orderedlistfile
+cat $unorderedlistfile >> $orderedlistfile
+
+sed -e "/$lead/,/$tail/{ /$lead/{p; r $orderedlistfile
+}; /$tail/p; d }"  "$wikiDir/Wiki_Sidebar.md"
