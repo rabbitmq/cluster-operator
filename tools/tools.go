98d19daadae39476b210f4ac5ee7a1b4d8c1a5ae
David Ansari <david.ansari@gmx.de>
2021-09-03 12:38:21 +0200

    Bump to latest kustomize v4

--- tools/tools.go
+++ tools/tools.go
@@ -11,5 +11,5 @@ import (
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kind"
-	_ "sigs.k8s.io/kustomize/kustomize/v3"
+	_ "sigs.k8s.io/kustomize/kustomize/v4"
 )
