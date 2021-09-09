fa825eff527a3a1e4dde6832e5dff43c957a397f
David Ansari <david.ansari@gmx.de>
2021-09-09 13:23:13 +0200

    Add more Vault TLS tests

--- docs/examples/vault-tls/test.sh
+++ docs/examples/vault-tls/test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-set -xeo pipefail
-
+set -eo pipefail
 
 assertTLS() {
-  kubectl exec vault-tls-server-0 -c rabbitmq -- openssl s_client -connect vault-tls.example.svc.cluster.local:"$1" -verify_return_error -CAfile /etc/rabbitmq-tls/ca.crt
+  kubectl exec vault-tls-server-0 -c rabbitmq -- openssl s_client -connect "$1" -CAfile /etc/rabbitmq-tls/ca.crt -verify_return_error
 }
 
-assertTLS 5671
-assertTLS 15671
+# Test TLS succeeds via client Service from server-0 to server-[0|1|2]
+assertTLS "vault-tls.$RABBITMQ_EXAMPLE_NAMESPACE.svc.cluster.local:5671"
+assertTLS "vault-tls.$RABBITMQ_EXAMPLE_NAMESPACE.svc.cluster.local:15671"
+
+# Test TLS succeeds via headless Service from server-0 to server-2
+assertTLS "vault-tls-server-2.vault-tls-nodes.$RABBITMQ_EXAMPLE_NAMESPACE:5671"
+assertTLS "vault-tls-server-2.vault-tls-nodes.$RABBITMQ_EXAMPLE_NAMESPACE:15671"
+
+# In this example, RabbitMQ gets only certs from Vault, but not the default user credentials.
+# Therefore, check that the default user K8s Secret is present.
+kubectl get secret vault-tls-default-user
