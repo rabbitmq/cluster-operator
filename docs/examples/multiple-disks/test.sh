e5cabe8bd34857b931133d0872924004182f0aa8
Connor Rogers <23215294+coro@users.noreply.github.com>
2022-02-09 16:08:22 +0000

    Fix multiple disks documented example test (#955)

--- docs/examples/multiple-disks/test.sh
+++ docs/examples/multiple-disks/test.sh
@@ -4,5 +4,5 @@ set -ex
 kubectl exec -t multiple-disks-server-0 -c rabbitmq -- rabbitmqctl environment > rabbitmq-environment.out
 
 grep 'data_dir,"/var/lib/rabbitmq/quorum-segments"' rabbitmq-environment.out
-grep "{wal_data_dir,'/var/lib/rabbitmq/quorum-wal'}" rabbitmq-environment.out
+grep 'wal_data_dir,"/var/lib/rabbitmq/quorum-wal"' rabbitmq-environment.out
 
