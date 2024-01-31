4c1c377168e9f789b4cbe2cd7bb66487e2fde2f6
mr-miles <miles.waller@gmail.com>
2024-01-31 22:19:19 +0000

    Update statefulset_test.go

--- internal/resource/statefulset_test.go
+++ internal/resource/statefulset_test.go
@@ -1098,7 +1098,7 @@ default_pass = {{ .Data.data.password }}
 						Expect(a).To(HaveKeyWithValue("vault.hashicorp.com/agent-inject-secret-ca.crt", instance.Spec.SecretBackend.Vault.TLS.PKIIssuerPath))
 
 						Expect(a).To(HaveKeyWithValue("vault.hashicorp.com/agent-inject-template-tls.crt", `
-{{- with secret "pki/issue/vmware-com" "common_name=myrabbit.com" "alt_names=myrabbit-server-0.myrabbit-nodes.foo-namespace,alt1,alt2" "ip_sans=9.9.9.9" -}}
+{{- with secret "pki/issue/vmware-com" "common_name=myrabbit.foo-namespace.svc "alt_names=myrabbit-server-0.myrabbit-nodes.foo-namespace" "ip_sans=" -}}
 {{ .Data.certificate }}
 {{- if .Data.ca_chain -}}
 {{- $lastintermediatecertindex := len .Data.ca_chain | subtract 1 -}}
@@ -1148,7 +1148,7 @@ default_pass = {{ .Data.data.password }}
 {{- end }}`))
 						Expect(a).To(HaveKeyWithValue("vault.hashicorp.com/agent-inject-template-ca.crt", `
 {{- with secret "pki-root/certs/ca" -}}
-{{ .Data.issuing_ca }}
+{{ .Data.certificate }}
 {{- end }}`))
 					})
 				})
