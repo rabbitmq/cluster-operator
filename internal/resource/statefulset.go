57af209c8a7a6ef68e44b2218641e01f306edb19
Michal Kuratczyk <michal.kuratczyk@broadcom.com>
2024-11-22 09:45:13 +0100

    Allow overriding readinessProbe and LivenessProbe
    There can only be 1 probe and without this change,
    specifying an exec readinessProbe in the spec.override would
    add it on top of the default TCP probe.
    
    As for livenessProbe, since there's currently no default,
    everything works without this change. However, we could add
    the default livenessProbe in the future and allowing this override
    doesn't break anything right now, so it's just future-proofing.
    
    Co-authored-by: joey <zchengjoey@gmail.com>

--- internal/resource/statefulset.go
+++ internal/resource/statefulset.go
@@ -286,6 +286,15 @@ func patchPodSpec(podSpec, podSpecOverride *corev1.PodSpec) (corev1.PodSpec, err
 		sortVolumeMounts(patchedPodSpec.Containers[0].VolumeMounts)
 	}
 
+	// allow overriding the liveness and readiness probes
+	// note: as of 2024, we don't set a default LivenessProbe
+	if rmqContainer.LivenessProbe != nil {
+		patchedPodSpec.Containers[0].LivenessProbe = rmqContainer.LivenessProbe
+	}
+	if rmqContainer.ReadinessProbe != nil {
+		patchedPodSpec.Containers[0].ReadinessProbe = rmqContainer.ReadinessProbe
+	}
+
 	// A user may wish to override the controller-set securityContext for the RabbitMQ & init containers so that the
 	// container runtime can override them. If the securityContext has been set to an empty struct, `strategicpatch.StrategicMergePatch`
 	// won't pick this up, so manually override it here.
