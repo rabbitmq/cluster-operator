4c2df1840f981ff55fbc9209bdbd0bbacfe968c5
Michal Kuratczyk <michal.kuratczyk@broadcom.com>
2025-02-25 16:26:52 +0100

    Add promtool for alert rule validation (#1824)

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -6,6 +6,7 @@ package tools
 import (
 	_ "github.com/elastic/crd-ref-docs"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
+	_ "github.com/prometheus/prometheus/cmd/promtool"
 	_ "github.com/sclevine/yj"
 	_ "golang.org/x/vuln/cmd/govulncheck"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
