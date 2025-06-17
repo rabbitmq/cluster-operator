30abb73606ba8176b6bd9cb0b80423c322be5302
Aitor Perez <1515757+Zerpet@users.noreply.github.com>
2025-06-17 17:07:47 +0100

    olm: test that operator deploys

--- olm.mk
+++ olm.mk
@@ -33,29 +33,31 @@ QUAY_IO_OPERATOR_IMAGE ?= quay.io/rabbitmqoperator/cluster-operator:latest
 deployment: ## Extract deployment spec. Customise using QUAY_IO_OPERATOR_IMAGE
 	mkdir -p tmp/
 	kustomize build config/installation/ | \
-		ytt -f- -f config/ytt/overlay-manager-image.yaml --data-value operator_image=$(QUAY_IO_OPERATOR_IMAGE) \
+		ytt -f- -f config/ytt/overlay-manager-image.yaml \
+			--data-value operator_image=$(QUAY_IO_OPERATOR_IMAGE) \
 			-f olm/templates/cluster-operator-namespace-scope-overlay.yml \
 		> tmp/cluster-operator.yml
 	yq '{"spec": .spec}' tmp/cluster-operator.yml > tmp/spec.yaml
 
-OLM_DIR = olm/manifests
+OLM_DIR = $(CURDIR)/olm/manifests
 $(OLM_DIR) :
 	mkdir -pv $@
 
 BUNDLE_CREATED_AT ?= $(shell date +'%Y-%m-%dT%H:%M:%S')
 BUNDLE_VERSION ?= 0.0.0
 olm-manifests: ## Render bundle manifests. Customise version using BUNDLE_VERSION and BUNDLE_CREATED_AT
-	ytt -f olm/templates/cluster-service-version-generator-openshift.yml \
-		--data-values-file tmp/role-rules.yaml \
-		--data-values-file tmp/spec.yaml \
+	ytt -f $(CURDIR)/olm/templates/cluster-service-version-generator-openshift.yml \
+		--data-values-file $(CURDIR)/tmp/role-rules.yaml \
+		--data-values-file $(CURDIR)/tmp/spec.yaml \
 		--data-value name="rabbitmq-cluster-operator" \
 		--data-value createdAt="$(BUNDLE_CREATED_AT)" \
 		--data-value image="$(QUAY_IO_OPERATOR_IMAGE)" \
 		--data-value version="$(BUNDLE_VERSION)" \
+		--data-value replaces="$(BUNDLE_REPLACES)" \
 		> $(OLM_DIR)/rabbitmq-cluster-operator.clusterserviceversion.yaml
 
 clean:
-	rm -f -v olm/manifests/*.y*ml
+	rm -f -v $(CURDIR)/olm/manifests/*.y*ml
 
 ###########
 ## Build ##
@@ -68,7 +70,7 @@ BUNDLE_IMAGE = $(REGISTRY)/$(IMAGE)
 
 .PHONY: docker-build docker-push
 docker-build: ## Build bundle container. Customise using REGISTRY and IMAGE
-	$(CONTAINER) build -t $(BUNDLE_IMAGE) -f olm/bundle.Dockerfile ./olm
+	$(CONTAINER) build -t $(BUNDLE_IMAGE) -f $(CURDIR)/olm/bundle.Dockerfile $(CURDIR)/olm
 
 docker-push: ## Push bundle container. Customise using REGISTRY and IMAGE
 	$(CONTAINER) push $(BUNDLE_IMAGE)
@@ -78,34 +80,40 @@ docker-push: ## Push bundle container. Customise using REGISTRY and IMAGE
 #############
 # This is used in tests
 
-olm/catalog:
+CATALOG_DIR = $(CURDIR)/olm/catalog
+$(CATALOG_DIR):
 	mkdir -p $@
 
 .PHONY: catalog-*
-catalog-replace-bundle: olm/catalog ## Generate catalog manifest. Customise using BUNDLE_IMAGE and BUNDLE_VERSION
-	ytt -f olm/templates/catalog-template.yaml \
+catalog-replace-bundle: $(CATALOG_DIR) ## Generate catalog manifest. Customise using BUNDLE_IMAGE and BUNDLE_VERSION
+	ytt -f $(CURDIR)/olm/templates/catalog-template.yaml \
 		--data-value name="rabbitmq-cluster-operator" \
 		--data-value image="$(BUNDLE_IMAGE)" \
 		--data-value version="$(BUNDLE_VERSION)" \
-	> olm/catalog/catalog.yaml
+	> $(CATALOG_DIR)/catalog.yaml
 
 CATALOG_IMAGE ?= rabbitmqoperator/test-catalog:latest
 catalog-build: ## Build catalog image. Customise using REGISTRY and CATALOG_IMAGE
-	$(CONTAINER) build -t $(REGISTRY)/$(CATALOG_IMAGE) --label "quay.expires-after=48h" -f olm/catalog.Dockerfile ./olm
+	$(CONTAINER) build -t $(REGISTRY)/$(CATALOG_IMAGE) --label "quay.expires-after=48h" -f $(CURDIR)/olm/catalog.Dockerfile ./olm
 
 catalog-push: ## Push catalog image. Customise using REGISTRY and CATALOG_IMAGE
 	$(CONTAINER) push $(REGISTRY)/$(CATALOG_IMAGE)
 
 catalog-deploy: ## Deploy a catalog source to an existing k8s
-	kubectl apply -f olm/assets/operator-group.yaml
-	ytt -f olm/assets/catalog-source.yaml --data-value image="$(REGISTRY)/$(CATALOG_IMAGE)" | kubectl apply -f-
-	kubectl apply -f olm/assets/subscription.yaml
+	kubectl apply -f $(CURDIR)/olm/assets/operator-group.yaml
+	ytt -f $(CURDIR)/olm/assets/catalog-source.yaml --data-value image="$(REGISTRY)/$(CATALOG_IMAGE)" | kubectl apply -f-
+	kubectl apply -f $(CURDIR)/olm/assets/subscription.yaml
 
 catalog-undeploy: ## Delete all catalog assets from k8s
-	kubectl delete -f olm/assets/subscription.yaml --ignore-not-found
-	kubectl delete -f ./olm/manifests/ --ignore-not-found
-	kubectl delete -f olm/assets/operator-group.yaml --ignore-not-found
-	ytt -f olm/assets/catalog-source.yaml --data-value image="$(REGISTRY)/$(CATALOG_IMAGE)" | kubectl delete -f- --ignore-not-found
+	kubectl delete -f $(CURDIR)/olm/assets/subscription.yaml --ignore-not-found
+	kubectl delete -f $(CURDIR)/olm/manifests/ --ignore-not-found
+	kubectl delete -f $(CURDIR)/olm/assets/operator-group.yaml --ignore-not-found
+	ytt -f $(CURDIR)/olm/assets/catalog-source.yaml --data-value image="$(REGISTRY)/$(CATALOG_IMAGE)" | kubectl delete -f- --ignore-not-found
 
 catalog-clean: ## Delete manifest files for catalog
-	rm -v -f ./olm/catalog/*.y*ml
+	rm -v -f $(CURDIR)/olm/catalog/*.y*ml
+
+catalog-all::catalog-replace-bundle
+catalog-all::catalog-build
+catalog-all::catalog-push
+catalog-all::catalog-deploy
