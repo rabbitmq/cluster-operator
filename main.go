c7ba24fc9ece8a331e80f6f9560af191b1ddcfa6
Michal Kuratczyk <michal.kuratczyk@broadcom.com>
2025-08-21 11:13:15 +0200

    Deploy RabbitMQ 4.1.3 by default

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func init() {
 func main() {
 	var (
 		metricsAddr             string
-		defaultRabbitmqImage    = "rabbitmq:4.1.1-management"
+		defaultRabbitmqImage    = "rabbitmq:4.1.3-management"
 		controlRabbitmqImage    = false
 		defaultUserUpdaterImage = "rabbitmqoperator/default-user-credential-updater:1.0.2"
 		defaultImagePullSecrets = ""
