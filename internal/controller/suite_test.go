dacd761f217534529faad9179e4602d92ebafa79
Amaan Ul Haq Siddiqui <amaanulhaq.s@outlook.com>
2026-05-28 20:36:06 +0500

    chore: reorder imports in controller suite_test.go per gofmt

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -26,8 +26,8 @@ import (
 	. "github.com/onsi/gomega"
 	rabbitmqv1beta1 "github.com/rabbitmq/cluster-operator/v2/api/v1beta1"
 	controllers "github.com/rabbitmq/cluster-operator/v2/internal/controller"
-	webhookv1beta1 "github.com/rabbitmq/cluster-operator/v2/internal/webhook/v1beta1"
 	"github.com/rabbitmq/cluster-operator/v2/internal/rabbitmqclient"
+	webhookv1beta1 "github.com/rabbitmq/cluster-operator/v2/internal/webhook/v1beta1"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
